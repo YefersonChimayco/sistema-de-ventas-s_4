25d858a457c6ecc5df23c7b16b60138434c21632
YefersonChimayco <chimaycohnj@gmail.com>
2024-11-26 09:11:56 -0500

    Update Producto.php

--- controller/Producto.php
+++ controller/Producto.php
@@ -44,20 +44,20 @@ if ($tipo=="listar") {
     if ($codigo=="" || $nombre=="" || $detalle=="" || $precio==""|| $stock==""|| $idcategoria=="" || $imagen=="" || $idproveedor=="") {
         $arr_Respuesta = array('status'=> false, 'mensaje'=>'error campos vacios');
     }else {
-        $arrProducto = $objProducto->registrarProducto($codigo, $nombre, $detalle,$precio, $stock, $idcategoria, $imagen, $idproveedor);
-        if ($arrProducto->id>0) {
-            $arr_Respuesta = array('status' => true, 'mensaje' =>'registro exitoso');
-            //cargar archivos
-            $archivo = $_FILES['imagen']['tmp_name'];
-            $destino = './assets/img_productos/';
-            $tipoArchivo = strtolower(pathinfo($_FILES["imagen"]["name"],PATHINFO_EXTENSION));
-            $nombre= $arrProducto->id.".".$tipoArchivo;    
-            if (move_uploaded_file($archivo,$destino.$nombre)) {
-                $arr_imagen  = $objProducto->actualizar_imagen($id,$nombre);
+        $archivo = $_FILES['imagen']['tmp_name'];
+        $destino = './assets/img_productos/';
+        $tipoArchivo = strtolower(pathinfo($_FILES["imagen"]["name"],PATHINFO_EXTENSION));
 
-            }
-            else {
-                $arr_Respuesta = array('status' => true, 'mensaje' =>'ERROR AL SUBIR IMAGEN');
+
+        $arrProducto = $objProducto->registrarProducto($codigo, $nombre, $detalle,$precio, $stock, $idcategoria, $imagen, $idproveedor ,$tipoArchivo);
+        if ($arrProducto->id_n>0) {
+            $newid = $arrProducto->id_n;
+            $arr_Respuesta = array('status' => true, 'mensaje' =>'registro exitoso');
+            $nombre=$arrProducto->id_n. ".". $tipoArchivo;
+            //cargar imagene
+            if (move_uploaded_file($archivo, $destino . '' . $nombre)) {
+            } else {
+                $arr_Respuesta = array('status' => true, 'mensaje' => 'Registro Exitoso, error al subir imagen');
             }
         } else {
             $arr_Respuesta = array('status' => false, 'mensaje'=>'error al SUBIR producto');
