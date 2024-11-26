9163a1fdf82064b5b994dd59a654ca6c98a5c7c2
YefersonChimayco <chimaycohnj@gmail.com>
2024-11-26 09:32:18 -0500

    Update header.php

--- views/include/header.php
+++ views/include/header.php
@@ -15,17 +15,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"> </script>
      <div class="container-fluid p-0" >
         <div class="cabecera">
-        <a href="<?php echo BASE_URL?>administracion" class="cre btn btn-danger m-20"  style="width: 100px;"   >Administrar</a>
-                <button type="button"   class="btn btn-success"  onclick="cerrar_sesion();">cerra sesion</button>
+        
                <a href="<?php echo BASE_URL?>menu" class="text_menu"> <div class="nosotros">Contactanos</div></a>
                <a href="<?php echo BASE_URL?>nosotros" class="text_menu"> <div class="nosotros">Nosotros</div></a>
                <a href="<?php echo BASE_URL?>login" class="text_menu"> <div class="Perfil"> login </a>
                <a href="<?php echo BASE_URL?>perfil" class="text_menu">  <img src="images/perfolincon.png" alt="" width="30" height="30">
-              
-                <div>
 
-                </div>
-
+               <a href="<?php echo BASE_URL?>administracion" class="cre btn btn-danger m-20"  style="width: 100px;"   >Administrar</a>
+               <button type="button"   class="btn btn-success"  onclick="cerrar_sesion();">cerra sesion</button>
 
                 
             </div>
